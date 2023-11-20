1f8a72457e21806cc4ac6a2c56f7599c1a872614
Carlos Alcazar <carlos@carlos.carlos>
2023-11-19 19:20:27 -0700

    fixed error with db path

--- src/gui/app.py
+++ src/gui/app.py
@@ -4,6 +4,7 @@ import sqlite3
 
 app = Flask(__name__)
 
+path = '/home/carlos/Desktop/Project/networks-project/UserCredentials.db'
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -11,7 +12,7 @@ def home():
 @app.route('/connect')
 def connect_to_database():
     try:
-        connection = sqlite3.connect('UserCredentials.db')
+        connection = sqlite3.connect(path)
         return 'Successfully connected to DB!'
     except sqlite3.Error as error:
         return f'ERROR: Failed to connect to DB: {error}'
@@ -19,7 +20,7 @@ def connect_to_database():
 @app.route('/view_table')
 def view_table():
     try:
-        connection = sqlite3.connect('UserCredentials.db')
+        connection = sqlite3.connect(path)
         cursor = connection.cursor()
         cursor.execute("SELECT * FROM credentials;")
         rows = cursor.fetchall()
